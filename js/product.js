a5142c58d65ffbd2fd0fc2d6fd822b1acc5c89ae
yur <adanyur@gmail.com>
2022-03-28 17:05:37 -0500

    add form dynamic

--- js/product.js
+++ js/product.js
@@ -65,14 +65,11 @@ data.addEventListener("click", ({ target }) => {
 });
 
 /*LISTADO DEL CARRITO*/
-
 const listProduct = () => {
   const data = getAddCart();
   let template = "";
   data.map(({ name, image, price }) => {
-    template += `
-
-                <div class="card" >
+    template += `<div class="card" >
                     <div class="card__img">
                       <img src="${image}" class="img__card">
                     </div>
@@ -80,12 +77,105 @@ const listProduct = () => {
                       <span>${name}</span>
                       <p>${price}</p>
                     </div>
-                </div>
-                  
-          
-                `;
+                </div>`;
   });
   document.getElementById("data").innerHTML = template;
 };
 
 listProduct();
+
+const FORM_DYNAMIC = [
+  {
+    label: "Name",
+    control: "input",
+    type: "text",
+    placeholder: "",
+    rules: {
+      required: true,
+    },
+    message: {},
+  },
+  {
+    label: "Phone",
+    control: "input",
+    type: "phone",
+    placeholder: "",
+    rules: {
+      required: true,
+    },
+  },
+  {
+    label: "Email",
+    control: "input",
+    type: "",
+    placeholder: "",
+    rules: {
+      required: true,
+    },
+  },
+  {
+    label: "Coment",
+    control: "textarea",
+    type: "",
+    placeholder: "",
+    rules: {
+      required: true,
+    },
+  },
+];
+
+const ID__FORM = document.getElementById("order");
+
+const input = ({ type, label, placeholder }) => {
+  let input = document.createElement("input");
+  input.type = type;
+  input.name = label;
+  input.name = placeholder;
+
+  let a = document.createElement("label");
+  a.innerHTML = label;
+
+  return input;
+};
+
+const textarea = ({ label }) => {
+  let textarea = document.createElement("textarea");
+  textarea.name = label;
+  return textarea;
+};
+
+const templateForm = () => {
+  const node = [];
+  document.getElementById("data").remove();
+  FORM_DYNAMIC.map((data) => {
+    const FORM = {
+      input: input(data),
+      textarea: textarea(data),
+    };
+
+    console.log(FORM[data.control].outerHTML);
+
+    node.push(FORM[data.control]);
+  });
+
+  document.getElementById("order").append(...node);
+};
+
+templateForm();
+
+/**Generar orden**/
+document.getElementById("generarOrden").addEventListener("click", (e) => {
+  let data = getAddCart();
+  console.log(data[0]);
+  document.getElementById("data").remove();
+});
+
+// $.ajax({
+//   url: `${URL}/order/`,
+//   type: "POST",
+//   contentType: "application/json",
+//   data: JSON.stringify(data[0]),
+//   success: (data) => {
+//     console.log(data);
+//   },
+// });
--- src/public/order/index.php
+++ src/public/order/index.php
@@ -4,19 +4,21 @@ require_once dirname(dirname(dirname(__FILE__))).'\controller\OrderController.ph
 $order = new OrderController();
 $data  = json_decode(file_get_contents('php://input'), true);
 
-switch ($_SERVER['REQUEST_METHOD']) {
-    case "GET":
-        die($order->index());
-        break;
-    case "POST":
-        die($order->store($data));
-        break;
-    case "PUT":
-        die($order->update($data));
-        break;
-    case "DELETE":
-        die($order->delete($data));
-        break;
-    default:
-        die($order->index());
-}
+echo json_encode($data);
+
+// switch ($_SERVER['REQUEST_METHOD']) {
+//     case "GET":
+//         die($order->index());
+//         break;
+//     case "POST":
+//         die($order->store($data));
+//         break;
+//     case "PUT":
+//         die($order->update($data));
+//         break;
+//     case "DELETE":
+//         die($order->delete($data));
+//         break;
+//     default:
+//         die($order->index());
+// }
